63627fb2c52355a180134369f1fe0e43b17fd2dc
YoonJieut <qmdldos@gmail.com>
2024-05-14 23:57:19 +0900

    feat : 컴포넌트 분리
    1. 제일 큰 html로 넣어본다.
    2. 정적 파일을 분류한다.
    3. 컴포넌트 분리
    4. 컴포넌트를 합치는 page 컴포넌트 추가

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Header(a, b) {
+  return (
+    <header>
+      <div className="headerBox">
+        <Nav />
+      </div>
+    </header>
+  );
+}
