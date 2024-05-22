12e8869e3f78dcba35d9e2afc28a2428720e6d04
YoonJieut <qmdldos@gmail.com>
2024-05-22 21:21:22 +0900

    Feedback : 수정사항 저장

--- frontend/src/components/main/KategorieTag.jsx
+++ frontend/src/components/main/KategorieTag.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function KategorieTag(){
-    return(
-        <ul>
-            <li>카테고리1</li>
-            <li>카테고리2</li>
-            <li>카테고리3</li>
-            <li>카테고리4</li>
-            <li>카테고리5</li>
-            <li>카테고리6</li>
-        </ul>
-    );
+export default function KategorieTag() {
+  // TODO : METADATA 파일을 따로 분리하면 편하다.
+  // 폴더 이름은 METADATA로 해도 좋고 검색해도 좋다.
+  const metaData = [
+    "카테고리1",
+    "카테고리2",
+    "카테고리3",
+    "카테고리4",
+    "카테고리5",
+    "카테고리6",
+  ];
+
+  return (
+    <ul>
+      {metaData.map((data, index) => {
+        return <li key={index}>{data}</li>;
+      })}
+    </ul>
+  );
 }
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,15 +3,13 @@ import Header from "../components/header/Header";
 import Main from "../components/main/Main";
 
 // style
-// import "../styles/common/reset.css";
-// import "../styles/common/common.css";
 import "../styles/main/main.css";
 
 export default function MainPage() {
   return (
     <>
-        <Header />
-        <Main />
+      <Header />
+      <Main />
     </>
   );
 }
