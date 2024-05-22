78624b73d1e94e95f869b8ea412149b6d4affe90
seulgi <seulgi@gmail.com>
2024-05-22 17:53:14 +0900

    Docs: style 수정
    commin, reset style은 공통으로 들어가는 스타일로 public/index.html에 경로를 넣어줌

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,13 +3,15 @@ import Header from "../components/header/Header";
 import Main from "../components/main/Main";
 
 // style
+// import "../styles/common/reset.css";
+// import "../styles/common/common.css";
 import "../styles/main/main.css";
 
 export default function MainPage() {
   return (
     <>
-      <Header />
-      <Main />
+        <Header />
+        <Main />
     </>
   );
 }
