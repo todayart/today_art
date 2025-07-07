3f6e00f82bb0021634d0ef6292a7785fd8ae9e20
YoonJunHyeon <qmdldos@gmail.com>
2025-07-07 14:27:30 +0900

    feat: Nav.jsx 리팩토링
    - 기본 컴포넌트를 반복문으로 작성
    
    - 설명 : svg를 meta.json으로 정리하는데 한계가 있는데 이는 컴포넌트를 문자열로 읽을 수 없기 때문에, 매핑 단계가 필요하기 때문이다.
    
    - memo : 동적 매핑이란 방법으로 구현하기 위해 리스트업

--- frontend/src/components/header/Nav.jsx
+++ frontend/src/components/header/Nav.jsx
@@ -1,48 +1,26 @@
 import React from "react";
 import { ReactComponent as HomeSvg } from "../../assets/common/home.svg";
-import { ReactComponent as UserSvg } from "../../assets/common/user.svg";
 import { ReactComponent as CalendarSvg } from "../../assets/common/calendar.svg";
-import { ReactComponent as QuestionSvg } from "../../assets/common/question.svg";
+import { ReactComponent as UserSvg } from "../../assets/common/user.svg";
 import { ReactComponent as WishSvg } from "../../assets/common/wish.svg";
+import { ReactComponent as QuestionSvg } from "../../assets/common/question.svg";
+import urlMeta from "../../contents/urlMeta.json";
 
 export default function Nav() {
+  // 컴포넌트 매핑
+  const icons = { HomeSvg, CalendarSvg, UserSvg, WishSvg, QuestionSvg };
+
   return (
     <nav>
       <ul className="navBox">
-        <li>
-          <a href="#">
-            HOME
-            <HomeSvg />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            CALENDAR
-            {/* calendaricon */}
-            <CalendarSvg />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            USER
-            {/* usericon */}
-            <UserSvg />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            WISH
-            {/* wishicon */}
-            <WishSvg />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            INFO
-            {/* infoicon */}
-            <QuestionSvg />
-          </a>
-        </li>
+        {urlMeta.navLinks.map((link) => {
+          const Icon = icons[link.icon];
+          return (
+            <li key={link.name}>
+              <a href={link.url}>{Icon && <Icon />}</a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
