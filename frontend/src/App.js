9f169ebf5b57a81b3317b088c28918a60032b33d
YoonJunHyeon <qmdldos@gmail.com>
2025-07-18 16:31:10 +0900

    feat : App.js route 추가
    - 각 라우트 별 렌더할 컴포넌트를 설정해줌

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
 import "react-datepicker/dist/react-datepicker.css";
-import TestPage from "./components/test/TestPage.jsx";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import TestPage from "components/test/TestPage";
+import EntryPage from "pages/EntryPage";
+import ListPage from "pages/ListPage";
+import DetailPage from "pages/DetailPage";
+import CalenderPage from "pages/CalenderPage";
 
 function App() {
-  return <TestPage />;
+  return (
+    <Router>
+      <Routes>
+        {/* 아직 개발단계기 때문에 많이 쓰는 테스트 페이지를 루트에 설정함 */}
+        <Route path="/" element={<TestPage />} />
+        <Route path="/entry" element={<EntryPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/calender" element={<CalenderPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
