cf68249baf5c5f147add5389f10f463277414d38
YoonJunHyeon <qmdldos@gmail.com>
2025-08-04 21:14:41 +0900

    feat : CalenderPage 구현
    - 설명 주석 변경, 데이터 프롭 연결
    
    - CalendarCell : children으로 구조 변경, innerCell 클래스 추가로 hover 스타일링 간단 구현

--- frontend/src/components/calendar/CalendarFixedCell.jsx
+++ frontend/src/components/calendar/CalendarFixedCell.jsx
@@ -4,9 +4,8 @@ import ExhibitionTag from "components/calendar/ExhibitionTag";
 export default function CalendarFixedCell({ exhibitions }) {
   // TODO : exhibitions 데이터를 받아서 각 셀에 전시 이름을 표시하도록 구현
   // 예시로 빈 객체를 사용하고 있습니다. 실제로는 API에서 데이터를 받아
-  // exhibitions 객체는 { "1m-start": "전시1", "1m-end": "전시2", ... } 형태로 되어 있어야 합니다.
+  // exhibitions 객체는 { "1m-start": ["전시1", ... ], "1m-end": ["전시2", ...] } 형태로 되어 있어야 합니다.
   // 각 키에 해당하는 값은 전시 이름입니다.
-  // 예시: exhibitions = { "1m-start": "전시1", "1m-end": "전시2", ... }
   // exhibitions 객체가 비어있으면 빈 셀을 렌더링합니다.
 
   return (
@@ -24,20 +23,21 @@ export default function CalendarFixedCell({ exhibitions }) {
           {label}
         </div>
       ))}
-      {/* TODO : 동적으로 쉘을 추가하도록 구현해봐야한다. */}
-      {/* api를 활용해 동적 쉘과 전시이름이 적힌 컴포넌트가 생성된다. */}
-
+      {/* TODO : 해당 CalenarCell이 클릭되면 .innerCell:hover가 발생함 이를 트리거로, id를 추적하여 open인지 close인지, 몇 월인지 배경 색을 바꾸는 옵션을 추가하면 ux가 상승할 것 같다. */}
       {/* 반복문 설명 : 2열부터 13열: 각 월에 대한 그리드 셀, 배열에 index만 필요하니 _를 사용해서 map을 활용함*/}
       {Array.from({ length: 12 }).map((_, monthIndex) =>
         Array.from({ length: 2 }).map((_, rowIndex) => {
           const id = `${monthIndex + 1}m-${rowIndex === 0 ? "start" : "end"}`;
           return (
-            <CalendarCell
-              key={id}
-              monthIndex={monthIndex}
-              rowIndex={rowIndex}
-              tag={exhibitions[id] && <ExhibitionTag name={exhibitions[id]} />}
-            />
+            <CalendarCell key={id} monthIndex={monthIndex} rowIndex={rowIndex}>
+              {/* exhibitions 객체에서 해당 월의 전시 이름을 가져와서 ExhibitionTag 컴포넌트로 렌더링 */}
+              {/* id가 key와 같다면 value를 name으로 전달하면 된다. */}
+              {/* { "1m-start": ["전시1", ... ], "1m-end": ["전시2", ...] } */}
+              {exhibitions[id] &&
+                exhibitions[id].map((exhibition, index) => (
+                  <ExhibitionTag key={index} name={exhibition} />
+                ))}
+            </CalendarCell>
           );
         })
       )}
--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -1,9 +1,8 @@
 import CalendarFixedCell from "components/calendar/CalendarFixedCell";
 import CalendarHeader from "components/calendar/CalendarHeader";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import "react-datepicker/dist/react-datepicker.css";
-
+import { fetchData } from "utils/fetchData";
 /**
  * 이 컴포넌트는 calendar 페이지의 메인 컴포넌트입니다.
  *
@@ -11,17 +10,28 @@ import "react-datepicker/dist/react-datepicker.css";
  *  1m-start, 1m-end, 2m-start, 2m-end ... 12m-start, 12m-end로 설정되어 있음
  */
 export default function CalendarPage() {
+  const [exhibitions, setExhibitions] = useState({});
   // useEffect나 커스텀훅으로 내용을 작성
   useEffect(() => {
     // 여기에 필요한 초기화 작업이나 API 호출 등을 작성할 수 있습니다.
     console.log("CalendarPage mounted");
+    fetchData("http://localhost:8000/api/calendar/")
+      .then((data) => {
+        console.log("Fetched calendar data:", data);
+        // 데이터를 상태에 저장하거나 필요한 작업을 수행합니다.
+        setExhibitions(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching calendar data:", error);
+      });
   }, []);
+
   return (
     <>
       {/* 캘린더 헤더 영역 */}
       <CalendarHeader />
       {/* 메인 캘린더 영역 (grid layout으로 13열 x 2행) */}
-      <CalendarFixedCell exhibitions={{}} />
+      <CalendarFixedCell exhibitions={exhibitions} />
     </>
   );
 }
