456418bb6f01ce4296e2c3cf91dd3f85866dd162
YoonJunHyeon <qmdldos@gmail.com>
2025-10-21 21:05:15 +0900

    feat : 로고 클릭시 list 페이지로 네비게이트

--- frontend/src/components/main/Logoimg.jsx
+++ frontend/src/components/main/Logoimg.jsx
@@ -1,8 +1,16 @@
 import TitleLogo from "assets/main/title_logo_bk.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Logoimg({ className = "" }) {
+  // Router 내비게이터
+  const navigate = useNavigate();
+  // 로고 클릭 시
+  const onLogoClick = () => {
+    navigate(`/list`);
+  };
+
   return (
-    <h1>
+    <h1 onClick={onLogoClick} style={{ cursor: "pointer" }}>
       <img
         src={TitleLogo}
         alt="TodayArt 홈페이지의 로고입니다."
