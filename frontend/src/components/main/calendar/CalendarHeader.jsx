5b534b95e7d255316d9219c5e2b7d989f631afdd
YoonJunHyeon <qmdldos@gmail.com>
2026-01-07 15:16:48 +0900

    feat: 캘린더 헤더 작업
    - 모바일 분기 추가
    
    - 캘린더 헤더에 내비게이션 버튼 추가 및 스타일 개선

--- frontend/src/components/main/calendar/CalendarHeader.jsx
+++ frontend/src/components/main/calendar/CalendarHeader.jsx
@@ -4,8 +4,15 @@ import { format } from "date-fns";
 
 import Logoimg from "components/main/Logoimg";
 import Header from "components/header/Header";
+import SvgButton from "components/common/SvgButton";
+
+import useMobile from "hooks/useMobile";
+
+import calendarArrow from "assets/common/mobile/calendarArrow.svg";
 
 export default function CalendarHeader({ activeMonth = null }) {
+  const isMobile = useMobile();
+
   return (
     <section className="commonHeader">
       <Header />
@@ -16,26 +23,65 @@ export default function CalendarHeader({ activeMonth = null }) {
           <span className="currentYear">{format(new Date(), "yyyy")}</span>
         </div>
         {/* 월 표시 그리드 파트 */}
-        <section className="monthGrid">
-          {[...Array(13)].map((_, index) => {
-            // 첫 쉘은 비움
-            if (index === 0) {
-              return <div key={index} className="monthCell"></div>;
-            }
-            const monthDate = new Date(2000, index - 1, 1);
-            const monthName = format(monthDate, "MMMM");
-            const isActive = activeMonth === index;
-            return (
-              <div
-                key={index}
-                className={`monthCell${isActive ? " is-active" : ""}`}
-              >
-                <span>{monthName}</span>
-                <span>{index < 10 ? "0" + index : index}</span>
-              </div>
-            );
-          })}
-        </section>
+        {isMobile ? (
+          <section className="monthGrid">
+            {[...Array(13)].map((_, index) => {
+              // 첫 쉘은 비움
+              if (index === 0) {
+                return <div key={index} className="monthCell"></div>;
+              }
+              const monthDate = new Date(2000, index - 1, 1);
+              const monthName = format(monthDate, "MMMM");
+              const isActive = activeMonth === index;
+              return (
+                // 월 표시
+                <div
+                  key={index}
+                  className={`monthCell${isActive ? " is-active" : ""}`}
+                >
+                  <SvgButton icon={calendarArrow} className="monthNavButton" />
+                  <div className="monthTitle">
+                    <span className="monthName">{monthName}</span>
+                    <span className="monthNumber">
+                      {index < 10 ? "0" + index : index}
+                    </span>
+                  </div>
+                  <SvgButton
+                    icon={calendarArrow}
+                    className="monthNavButton reverse"
+                  />
+                </div>
+              );
+            })}
+          </section>
+        ) : (
+          <section className="monthGrid">
+            {[...Array(13)].map((_, index) => {
+              // 첫 쉘은 비움
+              if (index === 0) {
+                return <div key={index} className="monthCell"></div>;
+              }
+              const monthDate = new Date(2000, index - 1, 1);
+              const monthName = format(monthDate, "MMMM");
+              const isActive = activeMonth === index;
+
+              return (
+                // 월 표시
+                <div
+                  key={index}
+                  className={`monthCell${isActive ? " is-active" : ""}`}
+                >
+                  <div className="monthTitle">
+                    <span className="monthName">{monthName}</span>
+                    <span className="monthNumber">
+                      {index < 10 ? "0" + index : index}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+        )}
       </div>
     </section>
   );
