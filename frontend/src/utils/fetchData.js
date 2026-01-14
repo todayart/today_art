3d025d383018f7ca2e45288fe9556973c6c1ba21
YoonJunHyeon <qmdldos@gmail.com>
2026-01-14 18:27:23 +0900

    - refact : useCalendarData 커스텀 훅 제작
    - race를 방지하기 위해 AC를 기반으로 작성, 시그널 사용을 위해 기존 fetchData에 2번째 인자에 option을 추가

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -7,8 +7,9 @@
  * @async
  * @function fetchData
  * @param {string} url - 데이터를 가져올 URL입니다.
+ * @param {Object} [options={}] - fetch 요청에 사용할 옵션 객체입니다.
  * @returns {Promise<any>} JSON 데이터를 포함하는 프로미스를 반환합니다.
- * @throws {Error} 네트워크 요청 실패 혹은 응답이 정상적이지 않을 경우 예외를 발생시킵니다.
+ * @throws {Error} 네트워크 요청 실패 시 예외를 발생시킵니다.
  *
  * @example
  * fetchData('https://api.example.com/data')
@@ -19,10 +20,10 @@
  *     console.error('데이터를 가져오는 중 오류 발생:', error);
  *   });
  */
-export const fetchData = async (url) => {
+export const fetchData = async (url, options = {}) => {
   try {
     console.log("FetchData Url :", url);
-    const response = await fetch(url);
+    const response = await fetch(url, options);
     if (!response.ok) {
       console.error("Fetch 에러:", response.statusText);
       // throw new Error("Network response was not ok");
